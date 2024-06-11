931db6c8cd3f2fb39f96df74425407fe9091b554
Aleksander Kret <aleksander.kret1@gmail.com>
2024-06-11 14:26:53 +0200

    Created habit's group model

---
+++ GoodBadHabitsTracker.Core/Models/Group.cs
@@ -0,0 +1,20 @@
+﻿using GoodBadHabitsTracker.Core.Models.Habit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodBadHabitsTracker.Core.Models
+{
+    public class Group
+    {
+        public Guid Id { get; private init; } = Guid.NewGuid();
+        public string Name { get; set; } = default!;
+        public Guid UserId { get; init; }
+        public User User { get; init; } = default!;
+        public List<GoodHabit> GoodHabits { get; init; } = new();
+        public List<QuitHabit> QuitHabits { get; init; } = new();
+        public List<LimitHabit> LimitHabits { get; init; } = new();
+    }
+}
