cdb26af1589cc6323f7d7dd0737821f6d86dcac9
Aleksander Kret <aleksander.kret@onet.pl>
2024-06-16 11:26:21 +0200

    Created custom exception class

---
+++ GoodBadHabitsTracker.Application/Exceptions/AppException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodBadHabitsTracker.Application.Exceptions
+{
+    public class AppException(HttpStatusCode code, object? errors ) : Exception
+    {
+        public HttpStatusCode Code { get; init; } = code;
+        public object? Errors { get; init; } = errors;
+    }
+}
