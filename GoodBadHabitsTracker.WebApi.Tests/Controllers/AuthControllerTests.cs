903f7d3a7dedafe25afb97aff01b1fb6a3dea053
Aleksander Kret <aleksander.kret@onet.pl>
2024-06-17 19:06:14 +0200

    Implemented changes in AuthControllerTests

--- GoodBadHabitsTracker.WebApi.Tests/Controllers/AuthControllerTests.cs
+++ GoodBadHabitsTracker.WebApi.Tests/Controllers/AuthControllerTests.cs
@@ -13,44 +13,33 @@ using GoodBadHabitsTracker.TestMisc;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Win32;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using GoodBadHabitsTracker.Application.Commands.Auth.Register;
 
 namespace GoodBadHabitsTracker.WebApi.Tests.Controllers
 {
    public class AuthControllerTests
    {
-       private readonly Mock<UserManager<User>> _userManagerMock;
-       private readonly Mock<RoleManager<UserRole>> _roleManagerMock;
-       private readonly Mock<IMediator> _mediatorMock;
-       private readonly AuthController _controller;
-       private readonly DataGenerator _dataGenerator;
-       public AuthControllerTests()
-       {
-           var userStoreMock = new Mock<IUserStore<User>>();
-           _userManagerMock = new Mock<UserManager<User>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
-           var roleStoreMock = new Mock<IRoleStore<UserRole>>();
-           _roleManagerMock = new Mock<RoleManager<UserRole>>(roleStoreMock.Object, null, null, null, null);
-           _mediatorMock = new Mock<IMediator>();
-           _controller = new AuthController(_userManagerMock.Object, _roleManagerMock.Object);
+        private readonly DataGenerator _dataGenerator;
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly AuthController _controller;
+        public AuthControllerTests()
+        {
             _dataGenerator = new DataGenerator();
-       }
-
-       [Fact]
-       public async Task Register_WhenRequestIsValid_AndRoleIsNotExists_CreateRole_AndReturnsCreatedAtAction()
-       {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new AuthController(_mediatorMock.Object);
+            
+        }
+        [Fact]
+        public async void Register_ValidRequest_ReturnsCreatedAtAction()
+        {
             //ARRANGE
             var request = _dataGenerator.SeedValidRegisterRequest();
 
-            _userManagerMock.Setup(x => x.CreateAsync(It.Is<User>(user => user.Id.GetType() == typeof(Guid)), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Success);
-            _roleManagerMock.Setup(x => x.RoleExistsAsync(It.IsAny<string>())).
-                ReturnsAsync(false);
-            _roleManagerMock.Setup(x => x.CreateAsync(It.IsAny<UserRole>()))
-            .ReturnsAsync(IdentityResult.Success);
-            _userManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Success);
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Command>(), default)).ReturnsAsync(new User { Email = request.Email, UserName = request.UserName });
 
             //ACT
-            var result = await _controller.Register(request) as CreatedAtActionResult;
+            var result = await _controller.Register(request, default) as CreatedAtActionResult;
             var actionName = result.ActionName;
             var routeValues = result.RouteValues;
             var value = result.Value;
