e098ccbe5a929ca7418bfdf5e38da888269af727
Aleksander Kret <aleksander.kret@onet.pl>
2024-06-19 18:36:11 +0200

    Written unit tests for user login action in AuthController

--- GoodBadHabitsTracker.TestMisc/DataGenerator.cs
+++ GoodBadHabitsTracker.TestMisc/DataGenerator.cs
@@ -1,24 +1,91 @@
 ﻿using Bogus;
 using GoodBadHabitsTracker.Application.DTOs.Auth.Request;
+using GoodBadHabitsTracker.Infrastructure.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GoodBadHabitsTracker.TestMisc
 {
-    public class DataGenerator
+    public class DataGenerator()
     {
         public RegisterRequest SeedValidRegisterRequest()
         {
             var registerRequestGenerator = new Faker<RegisterRequest>()
                 .RuleFor(rr => rr.Email, f => f.Internet.Email())
-                .RuleFor(rr => rr.Name, f => f.Internet.UserName())
+                .RuleFor(rr => rr.UserName, f => f.Internet.UserName())
                 .RuleFor(rr => rr.Password, f => f.Internet.Password())
                 .RuleFor(rr => rr.ConfirmPassword, (f, rr) => rr.Password);
 
             return registerRequestGenerator.Generate();
+        }
+
+        public LoginRequest SeedLoginRequest()
+        {
+            var loginRequestGenerator = new Faker<LoginRequest>()
+                .RuleFor(rr => rr.Email, f => f.Internet.Email())
+                .RuleFor(rr => rr.Password, f => f.Internet.Password());
+
+            return loginRequestGenerator.Generate();
+        }
+
+        public string SeedAccessToken(string email)
+        {
+            var headerGenerator = new Faker<JwtHeader>()
+                .CustomInstantiator(f => new JwtHeader()
+                {
+                    { "alg", "RS256" },
+                    { "typ", "JWT" }
+                });
+            var payloadGenerator = new Faker<JwtPayload>()
+                .CustomInstantiator(f => new JwtPayload()
+                {
+                    { JwtRegisteredClaimNames.Sub, f.Name.FirstName() },
+                    { JwtRegisteredClaimNames.Name, f.Name.LastName() },
+                    { JwtRegisteredClaimNames.Email, email },
+                    { JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString() },
+                    { "roles", "User" },
+                    { "userFingerprint", f.Random.String2(32) },
+                    { JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds() },
+                    { "authenticationMethod", "EmailPassword" },
+                    { "email_verified", f.Internet.Email() },
+                    { "iss", $"{f.Internet.Url}:{f.Internet.Port}" },
+                    { "aud", $"{f.Internet.Url}:{f.Internet.Port}" },
+                    { "exp", DateTime.UtcNow },
+                });
+
+            var signatureGenerator = new Faker<string>()
+                .CustomInstantiator(f => f.Random.String2(32));
+
+
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(new Faker<string>()
+                .CustomInstantiator(f => f.Random.String2(32))));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var header = (JwtHeader)headerGenerator;
+            var payload = (JwtPayload)payloadGenerator;
+
+
+            var token = new JwtSecurityToken(header, payload);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.WriteToken(token);
+            return jwtToken.ToString();
+        }
+
+        public string SeedRandomToken()
+        {
+            var tokenGenerator = new Faker<string>()
+                .CustomInstantiator(f => f.Random.String2(32));
+
+            var token = (string)tokenGenerator;
+            return token;
         }
     }
 }
--- GoodBadHabitsTracker.WebApi.Tests/Controllers/AuthControllerTests.cs
+++ GoodBadHabitsTracker.WebApi.Tests/Controllers/AuthControllerTests.cs
@@ -14,14 +14,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Win32;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using GoodBadHabitsTracker.Application.Commands.Auth.Register;
+
 using GoodBadHabitsTracker.Application.Exceptions;
 using System.Net;
 using GoodBadHabitsTracker.Application.DTOs.Auth.Request;
+using GoodBadHabitsTracker.Application.DTOs.Auth.Response;
+using System.Net.Http;
+using GoodBadHabitsTracker.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using FluentAssertions.Common;
+using System.Security.Authentication;
 
 namespace GoodBadHabitsTracker.WebApi.Tests.Controllers
 {
-   public class AuthControllerTests
+    using Register = Application.Commands.Auth.Register;
+    using Login = Application.Commands.Auth.Login;
+    public class AuthControllerTests
    {
         private readonly DataGenerator _dataGenerator;
         private readonly Mock<IMediator> _mediatorMock;
@@ -30,8 +39,13 @@ namespace GoodBadHabitsTracker.WebApi.Tests.Controllers
         {
             _dataGenerator = new DataGenerator();
             _mediatorMock = new Mock<IMediator>();
-            _controller = new AuthController(_mediatorMock.Object);
-            
+            _controller = new AuthController(_mediatorMock.Object)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
         }
         [Fact]
         public async void Register_ValidRequest_ReturnsCreatedAtAction()
@@ -39,7 +53,7 @@ namespace GoodBadHabitsTracker.WebApi.Tests.Controllers
             //ARRANGE
             var request = _dataGenerator.SeedValidRegisterRequest();
 
-            _mediatorMock.Setup(x => x.Send(It.IsAny<Command>(), default)).ReturnsAsync(new User { Email = request.Email, UserName = request.UserName });
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Register.Command>(), default)).ReturnsAsync(new User { Email = request.Email, UserName = request.UserName });
 
             //ACT
             var result = await _controller.Register(request, default) as CreatedAtActionResult;
@@ -61,14 +75,14 @@ namespace GoodBadHabitsTracker.WebApi.Tests.Controllers
             var request = _dataGenerator.SeedValidRegisterRequest();
             _controller.ModelState.AddModelError("Email", $"Email '{request.Email}' is already taken.");
 
-            _mediatorMock.Setup(x => x.Send(It.IsAny<Command>(), default)).ThrowsAsync(new AppException(HttpStatusCode.BadRequest, $"Failed to create user: Email '{request.Email}' is already taken."));
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Register.Command>(), default)).ThrowsAsync(new AppException(HttpStatusCode.BadRequest, $"Failed to create user: Email '{request.Email}' is already taken."));
 
             //ACT
             Func<Task> action = async () => await _controller.Register(request, default);
 
             //ASSERT
             _controller.ModelState.ErrorCount.Should().BeGreaterThan(0);
-            action.Should().ThrowAsync<AppException>();
+            action.Should().ThrowAsync<AppException>().Where(ex => ex.Code == HttpStatusCode.BadRequest).WithMessage($"Email '{request.Email}' is already taken.");
         }
 
         [Fact]
@@ -77,13 +91,63 @@ namespace GoodBadHabitsTracker.WebApi.Tests.Controllers
             //ARRANGE
             RegisterRequest request = null;
 
-            _mediatorMock.Setup(x => x.Send(It.IsAny<Command>(), default)).ReturnsAsync((User)null);
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Register.Command>(), default)).ReturnsAsync((User)null);
 
             //ACT
             Func<Task> action = async () => await _controller.Register(request, default);
 
             //ASSERT
-            action.Should().ThrowAsync<HttpRequestException>();
+            action.Should().ThrowAsync<HttpRequestException>().Where(ex => ex.StatusCode == HttpStatusCode.BadRequest);
+        }
+        [Fact]
+        public async void Login_CorrectCredentials_ReturnsOkWithAccessTokenRefreshToken()
+        {
+            //ARRANGE
+            var request = _dataGenerator.SeedLoginRequest();
+            var accessToken = _dataGenerator.SeedAccessToken(request.Email);
+            var refreshToken = _dataGenerator.SeedRandomToken();
+            var userFingerprint = _dataGenerator.SeedRandomToken();
+
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Login.Command>(), default)).ReturnsAsync(new LoginResponse(accessToken, refreshToken, userFingerprint));
+
+
+            //ACT
+            var result = await _controller.Login(request, default) as OkObjectResult;
+
+            //ASSERT
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
+            result.Value.Should().BeEquivalentTo(new { accessToken, refreshToken });
+            var properties = result.Value.GetType().GetProperties();
+            properties.All(p => p.PropertyType == typeof(string)).Should().BeTrue();
+            _controller.ControllerContext.HttpContext.Response.Headers.SetCookie.Should().BeEquivalentTo($"__Secure-Fgp={userFingerprint}; max-age=900; path=/; secure; samesite=strict; httponly");
+        }
+        [Fact]
+        public async void Login_InvalidCredentials_ReturnsUnauthorized()
+        {
+            //ARRANGE
+            var request = _dataGenerator.SeedLoginRequest();
+
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Login.Command>(), default)).ThrowsAsync(new AppException(HttpStatusCode.Unauthorized, "Invalid email or password"));
+
+            //ACT
+            Func<Task> action = async () => await _controller.Login(request, default);
+
+            //ASSERT
+            action.Should().ThrowAsync<AppException>().Where(ex => ex.Code == HttpStatusCode.Unauthorized).WithMessage("Invalid email or password");
+        }
+        [Fact]
+        public async void Login_NullRequest_ReturnsBadRequest()
+        {
+            //ARRANGE
+            LoginRequest request = null;
+
+            _mediatorMock.Setup(x => x.Send(It.IsAny<Login.Command>(), default)).ReturnsAsync((LoginResponse)null);
+
+            //ACT
+            Func<Task> action = async () => await _controller.Login(request, default);
+
+            //ASSERT
+            action.Should().ThrowAsync<HttpRequestException>().Where(ex => ex.StatusCode == HttpStatusCode.BadRequest);
         }
     }
 }
