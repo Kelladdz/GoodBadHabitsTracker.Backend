33fafb1859bde97cb19231c57280d12e9e0b09d3
Aleksander Kret <aleksander.kret@onet.pl>
2024-06-21 10:44:23 +0200

    Written unit test for user session model class

---
+++ GoodBadHabitsTracker.Core.Tests/Models/UserSessionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodBadHabitsTracker.Core.Models;
+using GoodBadHabitsTracker.TestMisc;
+using FluentAssertions;
+
+namespace GoodBadHabitsTracker.Core.Tests.Models
+{
+    public class UserSessionTests
+    {
+        private readonly DataGenerator _dataGenerator;
+        public UserSessionTests()
+        {
+            _dataGenerator = new DataGenerator();
+        }
+        [Fact]
+        public void ToString_ReturnsCorrectString()
+        {
+            //ARRANGE
+            var user = _dataGenerator.SeedUserSession();
+
+            var expectedString = $"Id: {user.Id}, Name: {user.UserName}, Email: {user.Email}, Roles: {user.Roles}";
+
+            //ACT
+            var result = user.ToString();
+
+            //ASSERT
+            result.Should().BeEquivalentTo(expectedString);
+        }
+    }
+}
