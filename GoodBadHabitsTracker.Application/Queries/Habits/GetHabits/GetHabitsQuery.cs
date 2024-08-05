4bca74cb87a67e55c39325fe7dafda40391c5c4a
Aleksander Kret <aleksander.kret@onet.pl>
2024-08-05 16:23:07 +0200

    Written validator for get habits query

--- GoodBadHabitsTracker.Application/Queries/Habits/GetHabits/GetHabitsQuery.cs
+++ GoodBadHabitsTracker.Application/Queries/Habits/GetHabits/GetHabitsQuery.cs
@@ -6,8 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using GoodBadHabitsTracker.Application.DTOs.Habit.Response;
 using GoodBadHabitsTracker.Application.DTOs.Habit.Request;
+using GoodBadHabitsTracker.Application.Abstractions.Messaging;
 
 namespace GoodBadHabitsTracker.Application.Queries.Habits.GetHabits
 {
-    public record GetHabitsQuery(GetHabitsRequest Request) : IRequest<HabitsResponse>;
+    public record GetHabitsQuery(GetHabitsRequest Request) : ICommand<HabitsResponse>;
 }
---
+++ GoodBadHabitsTracker.Application/Queries/Habits/GetHabits/GetHabitsQueryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using GoodBadHabitsTracker.Application.Queries.Habits.GetHabits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodBadHabitsTracker.Application.Queries.Habits.GetHabits
+{
+    public sealed class GetHabitsQueryValidator : AbstractValidator<GetHabitsQuery>
+    {
+        public GetHabitsQueryValidator()
+        {
+            RuleFor(x => x.Request.Date)
+                .Must(BeWithinNextSevenDays).WithMessage("The date must be within the next 7 days.");
+        }
+
+        private bool BeWithinNextSevenDays(DateOnly date)
+        {
+            var today = DateTime.Today;
+            var sevenDaysFromNow = DateOnly.FromDateTime(today.AddDays(7));
+            return date <= sevenDaysFromNow;
+        }
+
+    }
+}
+
